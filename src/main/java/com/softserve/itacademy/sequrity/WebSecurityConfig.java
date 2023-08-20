f1460daed705d0b6d4491daa07581ace66cbd00c
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-20 22:16:14 +0300

    created the project, LoginController, login-form.html, WebSecurityConfig

---
+++ src/main/java/com/softserve/itacademy/sequrity/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.softserve.itacademy.sequrity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class WebSecurityConfig {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public WebSecurityConfig(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                            .and()
+                    .formLogin()
+                    .loginPage("/login-form")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/home", true)
+                    .failureUrl("/login-form?error==true")
+                            .and()
+                    .logout()
+                    .permitAll()
+                    .logoutUrl("/logout")
+                    .deleteCookies("JSESSIONID");
+            return http.build();
+        }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new MyUserJdbcDetailsService(jdbcTemplate);
+    }
+
+}
