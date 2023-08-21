57ddd4c8cb6e88f01c9dff5493fd90b0316a4742
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-21 09:00:34 +0300

    refactored  WebSecurityConfig

--- src/main/java/com/softserve/itacademy/sequrity/WebSecurityConfig.java
+++ src/main/java/com/softserve/itacademy/sequrity/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
 @Configuration
@@ -46,7 +47,11 @@ public class WebSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new MyUserJdbcDetailsService(jdbcTemplate);
+        return new MyUserDetailsService(jdbcTemplate);
     }
 
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+        return new MyAuthenticationSuccessHandler();
+    }
 }
