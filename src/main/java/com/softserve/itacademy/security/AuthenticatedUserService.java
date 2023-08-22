2199410fd92bf9cf0e81dafd8e08e0e6b1b0ca6a
ruslana-blackbird-lab <rchumachenko@blackbird-lab.com>
2023-08-22 20:02:58 +0300

    working on TodoController

--- src/main/java/com/softserve/itacademy/security/AuthenticatedUserService.java
+++ src/main/java/com/softserve/itacademy/security/AuthenticatedUserService.java
@@ -1,6 +1,7 @@
 package com.softserve.itacademy.security;
 
 import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.ToDoService;
 import com.softserve.itacademy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,10 +13,21 @@ public class AuthenticatedUserService {
      @Autowired
      private UserService userService;
 
+     @Autowired
+     private ToDoService toDoService;
+
      public boolean hasId(long id){
           String username =  SecurityContextHolder.getContext().getAuthentication().getName();
           User user = userService.findByEmail(username);
           return user.getId()==id;
+     }
+
+     public boolean isOwner(long id){
+          String username =  SecurityContextHolder.getContext().getAuthentication().getName();
+          long userId = userService.findByEmail(username).getId();
+          //in progress
+          return true;
+
 
      }
 
