a8497cfeb2ccf02f4a5c40de4ce7983c89101e7e
Ruslana <38077693+springdryad@users.noreply.github.com>
2023-08-22 21:25:57 +0300

    Update UserController.java

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -3,16 +3,11 @@ package com.softserve.itacademy.controller;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 
 @Controller
 @RequestMapping("/users")
@@ -21,41 +16,35 @@ public class UserController {
     private final UserService userService;
     private final RoleService roleService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @Secured("ROLE_ADMIN")
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("user", new User());
         return "create-user";
     }
 
-
     @PostMapping("/create")
     public String create(@Validated @ModelAttribute("user") User user, BindingResult result) {
         if (result.hasErrors()) {
             return "create-user";
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(user.getPassword());
         user.setRole(roleService.readById(2));
         User newUser = userService.create(user);
         return "redirect:/todos/all/users/" + newUser.getId();
     }
 
-    @PreAuthorize("hasRole('ADMIN') or @authenticatedUserService.hasId(#id)")
     @GetMapping("/{id}/read")
     public String read(@PathVariable long id, Model model) {
         User user = userService.readById(id);
         model.addAttribute("user", user);
         return "user-info";
     }
-    @PreAuthorize("hasRole('ADMIN') or @authenticatedUserService.hasId(#id)")
+
     @GetMapping("/{id}/update")
     public String update(@PathVariable long id, Model model) {
         User user = userService.readById(id);
@@ -83,7 +72,6 @@ public class UserController {
     }
 
 
-    @Secured("ROLE_ADMIN")
     @GetMapping("/{id}/delete")
     public String delete(@PathVariable("id") long id) {
         userService.delete(id);
