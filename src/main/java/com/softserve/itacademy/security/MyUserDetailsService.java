c54c2e2727cf33898f663c9b6a5abd6906bb525d
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-22 10:34:15 +0300

    refactored MyUserDetailsService

--- src/main/java/com/softserve/itacademy/security/MyUserDetailsService.java
+++ src/main/java/com/softserve/itacademy/security/MyUserDetailsService.java
@@ -4,6 +4,7 @@ import com.softserve.itacademy.model.Role;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,53 +15,35 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class MyUserDetailsService implements UserDetailsService {
-    private UserService userService;
-    private RoleService roleService;
-
-     @Autowired
-     public void setUserService(UserService userService) {
-          this.userService=userService;
-     }
-
-     @Autowired
-     public void setRoleService(RoleService roleService) {
-          this.roleService=roleService;
-     }
+    private final UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.findByEmail(email);
         if (user == null) return null;
         else {
-           return new UserDetails() {
-              @Override
-              public Collection<? extends GrantedAuthority> getAuthorities() {
-                   Collection<Role> roles  = roleService.getAll();
-                   Set<GrantedAuthority> authorities
-                       = new HashSet<>();
-                   for (Role role: roles) {
-                        authorities.add(new SimpleGrantedAuthority(role.getName()));
-                   }
-
-                   return authorities;
-              }
-
-               @Override
-              public String getPassword() {return user.getPassword();}
-               @Override
-              public String getUsername() {return user.getEmail();}
-               @Override
-              public boolean isAccountNonExpired() {return true;}
-               @Override
-              public boolean isAccountNonLocked() {return true;}
-               @Override
-              public boolean isCredentialsNonExpired() {return true;}
-               @Override
-              public boolean isEnabled() {return true;}
-           };
+            return new UserDetails() {
+                @Override
+                public Collection<? extends GrantedAuthority> getAuthorities() {
+                    SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().toString());
+                    return List.of(authority);
+                }
+                @Override
+                public String getPassword() {return user.getPassword();}
+                @Override
+                public String getUsername() {return user.getEmail();}
+                @Override
+                public boolean isAccountNonExpired() {return true;}
+                @Override
+                public boolean isAccountNonLocked() {return true;}
+                @Override
+                public boolean isCredentialsNonExpired() {return true;}
+                @Override
+                public boolean isEnabled() {return true;}
+            };
         }
     }
 }
